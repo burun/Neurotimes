f05750288a622428fb9331705859b36569109bf6
Liang Zhou <liang.zhou@helsinki.fi>
2015-10-20 23:14:32 +0300

    Repair masonry display problem: add element sizing

--- static/js/neuro.js
+++ static/js/neuro.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
           $('.grid').masonry({
             // options
             itemSelector: '.grid-item',
-            columnWidth: '.grid-item',
+            columnWidth: ".grid-sizer",
           });
     });
 });
