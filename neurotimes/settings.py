43edae12a121abef8393f58478e29346b6f714dc
Liang Zhou <liang.zhou@helsinki.fi>
2015-10-31 13:46:50 +0200

    Modify setting.py

--- neurotimes/settings.py
+++ neurotimes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7vpwd$-(!z*gw%)agvh9ck#c#t7ynu-allezzsgg3zg^5eycc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '45.55.31.41',
