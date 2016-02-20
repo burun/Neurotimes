5f7f1469c1e3c3ee1688ca2d896d7a8b46206fc3
Liang Zhou <liang.zhou@helsinki.fi>
2016-02-21 00:15:26 +0200

    Add Infinite Scroll function in fragments

--- static/js/neuro.js
+++ static/js/neuro.js
@@ -1,9 +1,23 @@
-$(document).ready(function() {
-    $('.grid').imagesLoaded(function() {
-          $('.grid').masonry({
-            // options
-            itemSelector: '.grid-item',
-            columnWidth: ".grid-sizer",
-          });
+$(document).ready(function () {
+    var container = $('.scroll');
+    container.infinitescroll({
+    navSelector: '.pagination',
+    nextSelector: '.pagination a',
+    itemSelector: '.item',
+    animate: true,
+    bufferPx: 5,
+    extraScrollPx: 100,
+    loading: {
+        msgText: '<p>正在加载...</p>',
+        finishedMsg: '没有更多内容了',
+        img: '/media/img/ajax-loader.gif',
+    },
+    },
+    function (newElements) {
+        $(newElements).find('pre').each(function (i, e) {
+        hljs.highlightBlock(e)
+        });
     });
+
+
 });
